8c2f60557c4a0b760ee63d6334d337ac9f1c2375
s1l3nt78 <90134688+whiterabb17@users.noreply.github.com>
2022-10-24 08:01:15 +0200

    Update deepfire.go

--- deepfire-dev/deepfire.go
+++ deepfire-dev/deepfire.go
@@ -1,6 +1,6 @@
-// Package coldfire is a framework that provides functions
+// Package DeepFire is a framework that provides functions
 // for malware development that are mostly compatible with
-// Linux and Windows operating systems.
+// Linux and Windows operating systems - though there is some implementation for Darwin.
 package deepfire
 
 import (
